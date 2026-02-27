6666878e79680bd8f6916d24df3114dc4f00bc45
lab_services_student <st10456326@vcconnect.edu.za>
2026-02-27 23:24:35 +0200

    Home page and login\Signup

---
+++ BookingSystem/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
