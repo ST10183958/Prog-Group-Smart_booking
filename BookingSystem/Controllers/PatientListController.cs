846cbcd8b1526001e8eb351ddf2ce4dbf4b4d76c
njabuloemeris21 <ST10442968@myemeris.edu.za>
2026-04-06 11:29:07 +0200

    Patient List View

---
+++ BookingSystem/Controllers/PatientListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.Controllers
+{
+    public class PatientListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
